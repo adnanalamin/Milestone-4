738532864752ae0a87e0818b958ee812fcb38795
Al-Amin <116742755+adnanalamin@users.noreply.github.com>
2024-01-25 23:19:37 +0600

    add number.js file

---
+++ Introduction to javascript/number.js
@@ -0,0 +1,16 @@
+var orangePrice = 300;
+// var applePrice = parseInt("250");
+var applePrice = parseFloat("250");
+
+console.log(typeof applePrice)
+
+console.log(orangePrice + applePrice);
+
+var num = 0.1;
+var num2 = 0.2;
+
+var total = num+num2;
+console.log(total.toFixed(3))
+
+var someting = 50/0;
+console.log(someting)
