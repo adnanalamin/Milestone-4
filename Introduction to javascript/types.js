1f9a2d01d9d535c1557b03777d11a8fc75ac7c71
Al-Amin <116742755+adnanalamin@users.noreply.github.com>
2024-01-25 21:45:33 +0600

    added types.js file

---
+++ Introduction to javascript/types.js
@@ -0,0 +1,14 @@
+// String Type variable
+
+var name = "Al-amin";
+var address = "Shabuj Polli , chalna Bazar Dacope, Khulna";
+var country = 'Bangladesh';
+console.log(typeof country)
+
+// Boolean type
+
+var isRich = true;
+
+var passed = false;
+
+console.log(typeof passed)
--- Introduction to javascript/variable.js
+++ Introduction to javascript/variable.js
@@ -8,7 +8,10 @@ var weight = 94;
 var price = 38;
 var year = 1998;
 var money = 1100;
+money=1000;
 
 var age;
 
 console.log(age)
+console.log(price)
+console.log(money)
