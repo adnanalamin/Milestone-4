cfb7b05d658b2f3b3f9cf1318960146015e41ed7
Al-Amin <116742755+adnanalamin@users.noreply.github.com>
2024-01-25 23:50:32 +0600

    Shorthand file

---
+++ Introduction to javascript/shorthand.js
@@ -0,0 +1,18 @@
+var cap = 20;
+// cap = cap + 20;
+cap += 20;
+
+
+var cap = 20;
+// cap = cap - 20;
+cap -= 20;
+
+var cap = 20;
+// cap = cap * 20;
+cap *= 20;
+
+var cap = 20;
+// cap = cap / 20;
+cap /= 20;
+
+console.log(cap)
