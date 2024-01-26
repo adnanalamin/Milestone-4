165d1b062778be6fe3f0b20066968b18b3c511a4
Al-Amin <116742755+adnanalamin@users.noreply.github.com>
2024-01-26 20:57:01 +0600

    add comparison file

---
+++ Introduction to javascript/practices.js
@@ -0,0 +1,11 @@
+var myMoney = 1000;
+console.log("I have Money = " + myMoney);
+
+var apple = 300;
+var lichi = 400;
+
+var total = apple + lichi;
+console.log( "khoroch Korchi = " + total);
+
+var back = myMoney - total;
+console.log("Ferot pabo = " + back);
---
+++ conditional/comparision.js
@@ -0,0 +1,11 @@
+var applePrice = 300;
+var bananaPrice = 80;
+
+// console.log(applePrice != bananaPrice);
+
+
+// console.log(10 != '10');
+// console.log(10 !== '10');
+
+console.log(10 == '10'); //Only valu check
+console.log(10 === '10'); // Value and type cheack
