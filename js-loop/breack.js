420a103ebf4e4397e9248f293eeba70b695c2084
Al-Amin <116742755+adnanalamin@users.noreply.github.com>
2024-01-30 17:52:00 +0600

    break file added

---
+++ js-loop/breack.js
@@ -0,0 +1,14 @@
+// for (let i = 0; i < 1000; i++) {
+//   console.log(i);
+//   if (i >= 10) {
+//     break;
+//   }
+// }
+let n = 54;
+while(n > 45){
+    if(n < 49){
+        break;
+    }
+    console.log(n);
+    n--;
+}
