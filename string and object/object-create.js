75437123c170855af0e2b4321d7fc5b2cf66f7cb
Al-Amin <116742755+adnanalamin@users.noreply.github.com>
2024-02-02 00:09:19 +0600

    added some file

---
+++ string and object/object-create.js
@@ -0,0 +1,32 @@
+/**
+ * 
+ * যে কয়ভাবে ওবজেক্ট ক্রিয়েট করা যায়।
+ * 3 way to create Object
+ * 
+ * Using object literal
+ * By creating instance of Object directly
+ * By using constructor function
+ * 
+ */
+
+// Using object literal
+
+const mobile = {
+    brand: 'Infinix',
+    model:'15x',
+    color:'silver',
+    price:150000,
+    ram:'8gb',
+    memory:'128gb'
+}
+
+// console.log(mobile);
+
+
+// for (let phone in mobile){
+//     console.log(mobile[phone]);
+// }
+
+for(let i = 0; i > mobile.lenghth; i++){
+    console.log(i);
+}
