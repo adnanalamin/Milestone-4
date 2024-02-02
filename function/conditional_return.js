54a23a7215575ff439525094c9a4b97f0bf1fe15
Al-Amin <116742755+adnanalamin@users.noreply.github.com>
2024-02-02 21:34:37 +0600

    Added Conditional return file

---
+++ function/conditional_return.js
@@ -0,0 +1,21 @@
+function isOdd(number){
+    if(number%2===0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+console.log(isEven(11));
+console.log(isEven(10));
+
+function isOdd(number){
+    if(number%2===1){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+console.log(isOdd(11));
+console.log(isOdd(10));
