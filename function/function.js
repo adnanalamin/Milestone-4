1411ff51fdac9aa0cab19eee2adc0fd006998fcf
Al-Amin <116742755+adnanalamin@users.noreply.github.com>
2024-02-02 20:43:52 +0600

    Added Functions Folder

---
+++ function/function.js
@@ -0,0 +1,27 @@
+// Javascript Function
+
+function fanOffKor(){
+    console.log('Bosha theke uthe dara');
+    console.log('Switch ar kache ja');
+    console.log('Switch off kor');
+}
+// Function calling
+fanOffKor();
+
+// New function declar
+
+function brushTeeth(){
+    console.log('brush ne');
+    console.log('paste add kor');
+    console.log('brush gos and gos and fana ber kor');
+}
+
+// Function calling
+brushTeeth();
+console.log('-----------------------------');
+console.log('-----------------------------');
+console.log('-----------------------------');
+console.log('-----------------------------');
+console.log('Function new calling');
+
+brushTeeth();
---
+++ function/parameter.js
@@ -0,0 +1,28 @@
+// Declar function with parameter
+
+// Borgo ar man ber kora
+
+function squer(number) {
+  const borgo = number * number;
+  console.log(borgo);
+}
+squer(5);
+squer(100);
+squer(2);
+squer(6);
+
+// Add two number usign function two parameter
+
+function add(num1, num2) {
+  const total = num1 + num2;
+  console.log(total);
+}
+add(4, 5);
+
+// Add five number
+
+function addAll(a, b, c, d, e) {
+  const total = a + b + c + d + e;
+  console.log(total);
+}
+addAll(2, 2, 2, 2, 2);
